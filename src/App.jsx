bb4d6c7ae384843fb3af84aec92efe6d58527b21
MilanPataki <pataki.milan@telekimezotur.hu>
2026-09-15 14:24:32 +0200

    Adding a task

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { useState } from "react";
 
 function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
   function addTask(name) {
-    alert(name + " (called with a callback function)");
+    const newTask = { id: "id", name, completed: false };
+    setTasks([...tasks, newTask]);
   }
 
-  const taskList = props.tasks?.map((task) => (
+  const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
@@ -15,6 +19,7 @@ function App(props) {
       key={task.id}
     />
   ));
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
